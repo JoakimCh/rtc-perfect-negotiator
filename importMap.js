3c7f95ddd74ce4acd52757f022c2051113baea80
Joakim L. Christiansen <5984627+JoakimCh@users.noreply.github.com>
2025-01-27 21:35:37 +0100

    .

--- importMap.js
+++ importMap.js
@@ -1,18 +1,14 @@
 
 // just some magic:
-
-const root = document.location.host.endsWith('.github.io') ? 
-  // if hosted on GitHub
-  document.location.pathname.split('/').slice(0,2).join('/') :
-  // else if localhost
-  ''
+const url = new URL(document.currentScript.src)
+const dir = url.pathname.slice(0, url.pathname.lastIndexOf('/'))
 const script = document.createElement('script')
 script.type = 'importmap'
 script.textContent = JSON.stringify({
   imports: {
-    'rtc-perfect-negotiator': root+'/RTCPerfectNegotiator.js',
-    'wrapped-elements': root+'/wrapped-elements/wrapped-elements.js',
-    'tiny-peerserver-client': root+'/tiny-peerserver-client/PeerServerSignalingClient.js',
+    'rtc-perfect-negotiator': dir+'/RTCPerfectNegotiator.js',
+    'wrapped-elements':       dir+'/wrapped-elements/wrapped-elements.js',
+    'tiny-peerserver-client': dir+'/tiny-peerserver-client/PeerServerSignalingClient.js',
   }
 })
 document.head.append(script)
